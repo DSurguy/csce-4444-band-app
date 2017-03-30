481766e80ad3d3f67026431704d80d1eb85c555d
dsurguy <surguy.derek@gmail.com>
2017-03-30 02:27:38 +0000

    More login page prettiness, auto-resolving for now

--- client/js/app/pages/login.js
+++ client/js/app/pages/login.js
@@ -3,39 +3,93 @@
 /* global PageCtrl */
 /* global $ */
 
-/** Main Page **/
+/**
+ * PAGE
+ * */
 function LoginPage(app, data){
     Page.call(this, app, $('#loginPage')[0], LoginCtrl, LoginView);
 }
 LoginPage.prototype = Object.create(Page.prototype);
 LoginPage.prototype.constructor = LoginPage;
 
+/**
+ * CONTROLLER
+ * */
 function LoginCtrl(page){
     PageCtrl.call(this, page);
+    this.loggingIn = false;
 }
 LoginCtrl.prototype = Object.create(PageCtrl.prototype);
 LoginCtrl.prototype.constructor = LoginCtrl;
 
+LoginCtrl.prototype.login = function (username, password){
+    var defer = $.Deferred();
+    /*$.ajax('/login', {
+        type: 'POST',
+        data: $(this).serialize()
+    }).then(function (result){
+        alert('success');
+    }).fail(function (result){
+        alert('Failure');
+    });*/
+    setTimeout(function (){
+        defer.resolve();
+    },1000);
+    return defer.promise();
+};
+
+/**
+ * VIEWER
+ * */
 function LoginView(page){
     PageView.call(this, page);
 }
 LoginView.prototype = Object.create(PageView.prototype);
 LoginView.prototype.constructor = LoginView;
 LoginView.prototype.init = function (){
-    var pageElem = this.page.elem;
+    this.bindEvents();
+};
+
+LoginView.prototype.bindEvents = function (){
+    var pageElem = $(this.page.elem),
+        page = this.page;
         
-    $(pageElem).on('submit', 'form', function (e){
+    pageElem.on('submit', 'form', function (e){
         e.preventDefault();
         e.stopPropagation();
-        console.log($(this).serialize());
-        $.ajax('/login', {
-            type: 'POST',
-            data: $(this).serialize()
-        }).then(function (result){
-            alert('success');
-        }).fail(function (result){
-            alert('Failure');
+        //show spinner
+        var form = $(this),
+            submitButton = form.find('[type=submit]');
+        
+        //close any existing alerts
+        form.find('.alert').alert('close');
+        
+        //show the login thinking spinner
+        submitButton.html('<div class="fa fa-spinner animation-spin"></div>');
+        
+        //actually try to login
+        page.ctrl.login()
+        .then(function (){
+            submitButton.removeClass('btn-primary').addClass('btn-success');
+            submitButton.find('div').removeClass('fa-spinner animation-spin').addClass('fa-check');
+            //change page
+            setTimeout(function (){
+                page.app.changePage(MainPage, {user: {}});
+            }, 500);
+        }).fail(function (err){
+            submitButton.removeClass('btn-primary').addClass('btn-danger');
+            submitButton.find('div').removeClass('fa-spinner animation-spin').addClass('fa-times');
+            //change page
+            setTimeout(function (){
+                submitButton.html('Login').addClass('btn-primary').removeClass('btn-danger')
+            }, 1000);
+            //display login failure
+            form.prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert">'
+              +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                +'<span aria-hidden="true">&times;</span>'
+              +'</button>'
+              +'<strong>Login Failed!</strong> Username or password was incorrect.'
+            +'</div>');
         });
     });
-    
-};
+}
