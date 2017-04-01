53a4475f19d4678b5282e126604fe136f4abbd7e
dsurguy <surguy.derek@gmail.com>
2017-04-01 22:28:12 +0000

    Added form submission logic for /bands/register

--- static/js/app/pages/registerBand.js
+++ static/js/app/pages/registerBand.js
@@ -11,9 +11,23 @@ RegisterBandPage.prototype.constructor = RegisterBandPage;
 
 function RegisterBandCtrl(page){
     PageCtrl.call(this, page);
+    this.registering = false;
 }
 RegisterBandCtrl.prototype = Object.create(PageCtrl.prototype);
 RegisterBandCtrl.prototype.constructor = RegisterBandCtrl;
+RegisterBandCtrl.prototype.login = function (form){
+    var defer = $.Deferred();
+    $.ajax({
+        url: '/api/bands/register',
+        type: 'POST',
+        data: $(form).serialize()
+    }).then(function (result){
+        defer.resolve();
+    }).fail(function (result){
+        defer.reject();
+    });
+    return defer.promise();
+};
 
 function RegisterBandView(page){
     PageView.call(this, page);
@@ -25,5 +39,60 @@ RegisterBandView.prototype.init = function (){
 };
 
 RegisterBandView.prototype.bindEvents = function (){
-    var page = $(this.page);
+    var page = this.page,
+        pageElem = $(this.page.elem);
+    
+    pageElem.on('submit', 'form', function (e){
+        e.preventDefault();
+        e.stopPropagation();
+        if( page.ctrl.registering ){
+            return false;
+        }
+        else{
+            page.ctrl.registering = true;
+        }
+        
+        //show spinner
+        var form = $(this),
+            submitButton = form.find('[type=submit]');
+        
+        //close any existing alerts
+        form.find('.alert').alert('close');
+        
+        //show the login thinking spinner
+        submitButton.html('<div class="fa fa-spinner animation-spin"></div>');
+        
+        //actually try to login
+        page.ctrl.login(this)
+        .then(function (){
+            submitButton.removeClass('btn-primary').addClass('btn-success');
+            submitButton.find('div').removeClass('fa-spinner animation-spin').addClass('fa-check');
+            //display register failure
+            form.prepend('<div class="alert alert-success alert-dismissible fade show" role="alert">'
+              +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                +'<span aria-hidden="true">&times;</span>'
+              +'</button>'
+              +'<strong>Registration Successful!</strong> Redirecting in 2 seconds...'
+            +'</div>');
+            //change page
+            setTimeout(function (){
+                window.location = '/bands';
+            }, 2000);
+        }).catch(function (err){
+            submitButton.removeClass('btn-primary').addClass('btn-danger');
+            submitButton.find('div').removeClass('fa-spinner animation-spin').addClass('fa-times');
+            //change page
+            setTimeout(function (){
+                submitButton.html('Login').addClass('btn-primary').removeClass('btn-danger');
+                page.ctrl.registering = false;
+            }, 1000);
+            //display login failure
+            form.prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert">'
+              +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                +'<span aria-hidden="true">&times;</span>'
+              +'</button>'
+              +'<strong>Band Registration Failed!</strong>'
+            +'</div>');
+        });
+    });
 };
