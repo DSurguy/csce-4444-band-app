e20b619871d62831b1372a49143993eff135514e
Tara Boyle <tboyle621@gmail.com>
2017-04-29 19:51:08 +0000

    new file:   client/pages/userProfile.js

---
+++ client/pages/userProfile.js
@@ -0,0 +1,66 @@
+/* global Page */
+/* global PageView */
+/* global PageCtrl */
+/* global MenuComponent */
+/* global $ */
+
+function ProfilePage(app, data){
+    Page.call(this, app, $('#ProfilePage')[0], ProfileCtrl, BandView, {
+        menu: new MenuComponent(app, {
+            element: '.menu-button-container'
+        })
+    });
+}
+ProfilePage.prototype = Object.create(Page.prototype);
+ProfilePage.prototype.constructor = ProfilePage;
+
+function ProfileCtrl(page){
+    PageCtrl.call(this, page);
+    this.band = {};
+}
+ProfileCtrl.prototype = Object.create(PageCtrl.prototype);
+ProfileCtrl.prototype.constructor = ProfileCtrl;
+ProfileCtrl.prototype.init = function (){
+    var url = window.location.pathname;
+    var id = url.split('/')[ url.split('/').indexOf('bands')+1];
+
+    //var id = window.location.search.substr(1);
+    var defer = $.Deferred();
+    var that = this;
+    $.ajax('/api/bands/' + id, {
+        method: 'GET'
+    }).then(function (data){
+        that.profile = data;
+        defer.resolve();
+    }).catch(defer.reject);
+    
+    return defer.promise();
+};
+
+function BandView(page){
+    PageView.call(this, page);
+}
+BandView.prototype = Object.create(PageView.prototype);
+BandView.prototype.constructor = BandView;
+BandView.prototype.init = function (){
+    var bandElem = $(this.page.elem).find('.band');
+    bandElem.append('<h2 class="card-title">My Band</h2>');
+    bandElem.append('<div class="card-block"></div>');
+    
+    var bandInfoElem = bandElem.find('.card-block');
+    bandInfoElem.append(''
+        +'<p class="info card-text"><strong>Band Name</strong>: '+this.page.ctrl.band.bandName+'</p>'
+        +'<p class="info card-text"><strong>Owner</strong>: '+this.page.ctrl.band.ownerName+'</p>'
+        +'<p class="info card-text"><strong>Description</strong>: '+this.page.ctrl.band.description+'</p>'
+    );
+    
+    this.bindEvents();
+};
+
+BandView.prototype.bindEvents = function (){
+    var pageElem = $(this.page.elem);
+
+    pageElem.on('click', '.applications', function (e){
+        window.location = window.location.pathname+'/applications';
+    });
+};
